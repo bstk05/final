4dd6caf8fdc16b056815c5bc31f87add9d15607f
12965 <12965@DESKTOP-FLIBO64>
2017-05-21 14:05:18 -0400

    1

--- RocketBLL/src/test/java/rocketBase/rate_test.java
+++ RocketBLL/src/test/java/rocketBase/rate_test.java
@@ -14,5 +14,18 @@ public class rate_test {
 	//TODO - RocketBLL rate_test
 	//		Check to see if a RateException is thrown if there are no rates for a given
 	//		credit score
+	
+	@Test
+	public void testRate() throws RateException{
+		
+		
+		assertFalse(RateBLL.getRate(600) == 8);
+		assertTrue(RateBLL.getRate(800) == 3.5);
+	}
+	
+	@Test
+	public void test() {
+		assert(1==1);
+	}
 
 }
