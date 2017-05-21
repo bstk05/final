772df1132d1582d6693187b3c022a68b10b04eb4
12965 <12965@DESKTOP-FLIBO64>
2017-05-21 13:45:42 -0400

    1

--- RocketBLL/src/main/java/exceptions/RateException.java
+++ RocketBLL/src/main/java/exceptions/RateException.java
@@ -1,6 +1,12 @@
 package exceptions;
 
+import rocketDomain.RateDomainModel;
+
 public class RateException extends Exception {
+
+	public RateException(RateDomainModel rate_domain_model1) {
+		// TODO Auto-generated constructor stub
+	}
 
 	//	TODO - RocketBLL RateException - RateDomainModel should be an attribute of RateException
 	//	* Add RateRomainModel as an attribute
--- RocketBLL/src/main/java/rocketBase/RateBLL.java
+++ RocketBLL/src/main/java/rocketBase/RateBLL.java
@@ -9,25 +9,36 @@ import rocketDomain.RateDomainModel;
 
 public class RateBLL {
 
+	private static double Rates = 0;
 	private static RateDAL _RateDAL = new RateDAL();
 	
 	static double getRate(int GivenCreditScore) throws RateException 
 	{
-		double dInterestRate = 0;
-		
-		//TODO - RocketBLL RateBLL.getRate - make sure you throw any exception
-		
-		//		Call RateDAL.getAllRates... this returns an array of rates
-		//		write the code that will search the rates to determine the 
-		//		interest rate for the given credit score
-		//		hints:  you have to sort the rates...  you can do this by using
-		//			a comparator... or by using an OrderBy statement in the HQL
-		
-		
-		//TODO - RocketBLL RateBLL.getRate
-		//			obviously this should be changed to return the determined rate
 		
 		ArrayList<RateDomainModel> rates = RateDAL.getAllRates();
+		
+		double dInterestRate = 0;
+		
+		RateDomainModel rate_domain_model1 = new RateDomainModel();
+		
+		
+		for (RateDomainModel RD : rates){
+			
+			if (GivenCreditScore >= RD.getiMinCreditScore()) {
+				
+				Rates = RD.getdInterestRate();
+			}
+			
+			if (Rates == 0){
+				
+				throw new RateException(rate_domain_model1);
+				
+			}
+		}
+		return Rates;
+		
+		
+		
 		
 
 		//TODO: Filter the ArrayList...  look for the correct rate for the given credit score.
@@ -45,14 +56,17 @@ public class RateBLL {
 	
 	
 	
-	
-	
-	//TODO - RocketBLL RateBLL.getPayment 
-	//		how to use:
-	//		https://poi.apache.org/apidocs/org/apache/poi/ss/formula/functions/FinanceLib.html
+
 	
 	public static double getPayment(double r, double n, double p, double f, boolean t)
 	{		
+		if (r != 0){
+			(p*(1+r)^n + y(1+rt)((1+r)^n-1)/r + f) == 0;
+		}
+		if (r == 0){
+			(n*y + p + f) == 0;  
+			
+		}
 		return FinanceLib.pmt(r, n, p, f, t);
 	}
 }
