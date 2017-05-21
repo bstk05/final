e08e5ed10959f2c65473e9e673eb77b372294dce
12965 <12965@DESKTOP-FLIBO64>
2017-05-21 13:56:43 -0400

    1

--- RocketBLL/src/main/java/exceptions/RateException.java
+++ RocketBLL/src/main/java/exceptions/RateException.java
@@ -3,10 +3,21 @@ package exceptions;
 import rocketDomain.RateDomainModel;
 
 public class RateException extends Exception {
+	
+	private RateDomainModel rate_domain_model;
 
-	public RateException(RateDomainModel rate_domain_model1) {
-		// TODO Auto-generated constructor stub
+	public RateDomainModel getRate_domain_model() {
+		return rate_domain_model;
 	}
+
+	public void setRate_domain_model(RateDomainModel rate_domain_model) {
+		
+		this.rate_domain_model = rate_domain_model;
+	}	
+	public RateException(RateDomainModel rate_domain_model){
+		this.rate_domain_model = rate_domain_model;
+	}
+	
 
 	//	TODO - RocketBLL RateException - RateDomainModel should be an attribute of RateException
 	//	* Add RateRomainModel as an attribute
